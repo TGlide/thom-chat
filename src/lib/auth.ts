8d500cffb0c546ad34d58edc01be94e2b7ac6609
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-06-13 18:11:17 +0100

    github integration wip

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { convexAdapter } from '@better-auth-kit/convex';
 import { ConvexHttpClient } from 'convex/browser';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 const convexClient = new ConvexHttpClient(process.env.CONVEX_SITE_URL!);
 
@@ -9,10 +8,10 @@ export const auth = betterAuth({
 	secret: process.env.BETTER_AUTH_SECRET!,
 	database: convexAdapter(convexClient),
 	socialProviders: {
-		github: {
-			clientId: GITHUB_CLIENT_ID,
-			clientSecret: GITHUB_CLIENT_SECRET
-		}
+		// github: {
+		// 	clientId: process.env.GITHUB_CLIENT_ID!,
+		// 	clientSecret: process.env.GITHUB_CLIENT_SECRET!
+		// }
 	},
 	plugins: []
 });
