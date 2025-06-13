f6239792c44c1aed8c4416e880f7729776dec25e
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-13 12:37:14 -0500

    add locals call

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.auth = () => auth.api.getSession({ headers: event.request.headers });
+
 	return svelteKitHandler({ event, resolve, auth });
 };
