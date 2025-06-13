f2fc79e8109cfbba95191a9c25c6a306c120a2a2
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-13 10:55:57 -0500

    add auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from 'better-auth';
+import { convexAdapter } from '@better-auth-kit/convex';
+import { ConvexHttpClient } from 'convex/browser';
+
+const convexClient = new ConvexHttpClient(process.env.CONVEX_SITE_URL!);
+
+export const auth = betterAuth({
+	secret: process.env.BETTER_AUTH_SECRET!,
+	database: convexAdapter(convexClient),
+	plugins: []
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+    import { authClient } from "$lib/backend/auth/client";
+</script>
+
+<Button onclick={}>
+
+</Button>
