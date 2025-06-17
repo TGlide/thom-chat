f80d8f6ef746da1b441390fd9e1d6d0fd1bc234f
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-17 13:16:03 -0500

    new chat shortcut

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,18 @@
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { models } from '$lib/state/models.svelte';
 	import GlobalModal from '$lib/components/ui/modal/global-modal.svelte';
+	import { shortcut } from '$lib/actions/shortcut.svelte';
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
 
 	setupConvex(PUBLIC_CONVEX_URL);
 	models.init();
 </script>
+
+<svelte:window
+	use:shortcut={{ ctrl: true, shift: true, key: 'o', callback: () => goto('/chat') }}
+/>
 
 <ModeWatcher />
 {@render children()}
