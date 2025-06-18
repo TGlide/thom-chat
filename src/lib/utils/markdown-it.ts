aefd2f4a35f74c19b64404db436a9788d78744dd
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-06-18 19:40:10 +0100

    copy btn

--- src/lib/utils/markdown-it.ts
+++ src/lib/utils/markdown-it.ts
@@ -1,4 +1,5 @@
 import { fromAsyncCodeToHtml } from '@shikijs/markdown-it/async';
+import { h } from 'hastscript';
 import MarkdownItAsync from 'markdown-it-async';
 import { codeToHtml } from 'shiki';
 import DOMPurify from 'isomorphic-dompurify';
@@ -14,6 +15,28 @@ md.use(
 				light: 'github-light-default',
 				dark: 'github-dark-default',
 			},
+			transformers: [
+				{
+					name: 'shiki-transformer-copy-button',
+					pre(node) {
+						const button = h(
+							'button',
+							{
+								class: 'copy',
+								'data-code': this.source,
+								onclick: `
+          navigator.clipboard.writeText(this.dataset.code);
+          this.classList.add('copied');
+          setTimeout(() => this.classList.remove('copied'), ${3000})
+        `,
+							},
+							[h('span', { class: 'ready' }), h('span', { class: 'success' })]
+						);
+
+						node.children.push(button);
+					},
+				},
+			],
 		}
 	)
 );
