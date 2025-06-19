06d7db088a13221cc835b5c7f0dcfa002f38c1a3
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-06-19 18:48:49 +0100

    fix icons

--- src/lib/components/icons/branch-and-regen.svelte
+++ src/lib/components/icons/branch-and-regen.svelte
@@ -6,47 +6,38 @@
 </script>
 
 <svg
-	class={cn('size-4 fill-current', className)}
+	class={cn('size-4', className)}
+	width="24"
+	height="24"
 	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	version="1.1"
 	xmlns="http://www.w3.org/2000/svg"
 	{...rest}
 >
-	<path
-		d="M2.96132 21.09V4.55M2.96132 4.55V2.64M2.96132 4.55V5.78C2.96132 7.39 4.17132 8.89 6.16132 9.72L10.7413 11.64C12.7213 12.47 13.9413 13.96 13.9413 15.58V19.42"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-	<path
-		d="M17.4713 17.59L14.0613 21.25L10.4013 17.84"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-	<path
-		d="M20.8382 6.76662C20.8382 5.5946 20.3727 4.47058 19.5439 3.64183C18.7152 2.81309 17.5911 2.3475 16.4191 2.3475C15.1837 2.35215 13.9979 2.83421 13.1097 3.69288L12 4.80257"
-		stroke="currentColor"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-	<path
-		d="M12 2.3475V4.80257H14.4551"
-		stroke="currentColor"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-	<path
-		d="M12 6.76662C12 7.93865 12.4656 9.06267 13.2943 9.89141C14.1231 10.7202 15.2471 11.1857 16.4191 11.1857C17.6545 11.1811 18.8403 10.699 19.7285 9.84037L20.8382 8.73068"
-		stroke="currentColor"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
-	<path
-		d="M18.3832 8.73067H20.8382V11.1857"
-		stroke="currentColor"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
+	<g transform="translate(-3.8357017)">
+		<path
+			d="m 6.02,5.78 m 0,15.31 V 4.55 m 0,0 V 2.64 m 0,3.14 V 4.55 m 0,1.23 c 0,1.61 1.21,3.11 3.2,3.94 l 4.58,1.92 c 1.98,0.83 3.2,2.32 3.2,3.94 v 3.84"
+		/>
+		<path d="m 20.53,17.59 -3.41,3.66 -3.66,-3.41" />
+	</g>
+	<g
+		transform="matrix(0.50200343,0,0,0.50200343,8.639999,3.1460103)"
+		style="stroke-width:2.99599547;stroke-dasharray:none"
+	>
+		<path
+			d="M 28,7 A 9,9 0 0 0 19,-2 9.75,9.75 0 0 0 12.26,0.74 L 10,3"
+			style="stroke-width:2.99599547;stroke-dasharray:none"
+		/>
+		<path d="m 10,-2 v 5 h 5" style="stroke-width:2.99599547;stroke-dasharray:none" />
+		<path
+			d="m 10,7 a 9,9 0 0 0 9,9 9.75,9.75 0 0 0 6.74,-2.74 L 28,11"
+			style="stroke-width:2.99599547;stroke-dasharray:none"
+		/>
+		<path d="m 23,11 h 5 v 5" style="stroke-width:2.99599547;stroke-dasharray:none" />
+	</g>
 </svg>
--- src/lib/components/icons/branch.svelte
+++ src/lib/components/icons/branch.svelte
@@ -6,23 +6,24 @@
 </script>
 
 <svg
-	class={cn('size-4 fill-current', className)}
+	class={cn('size-4 ', className)}
+	width="24"
+	height="24"
 	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	version="1.1"
+	id="svg2"
 	xmlns="http://www.w3.org/2000/svg"
 	{...rest}
 >
+	<defs id="defs2" />
 	<path
-		d="M6.02 21.09V4.55M6.02 4.55V2.64M6.02 4.55V5.78C6.02 7.39 7.23 8.89 9.22 9.72L13.8 11.64C15.78 12.47 17 13.96 17 15.58V19.42"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
+		d="M6.02,5.78m0,15.31V4.55m0,0v-1.91m0,3.14v-1.23m0,1.23c0,1.61,1.21,3.11,3.2,3.94l4.58,1.92c1.98,.83,3.2,2.32,3.2,3.94v3.84"
+		id="path1"
 	/>
-	<path
-		d="M20.53 17.59L17.12 21.25L13.46 17.84"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	/>
+	<path d="M20.53,17.59l-3.41,3.66-3.66-3.41" id="path2" />
 </svg>
