d1128c413cadea0c94e34de992f9b2cd66f9834f
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-20 11:23:00 -0500

    hopefully fix merge conflict

--- src/lib/components/model-picker/model-picker.svelte
+++ src/lib/components/model-picker/model-picker.svelte
@@ -360,7 +360,7 @@
 													<EyeIcon class="size-3" />
 												</div>
 											{/snippet}
-											Supports image analysis
+											Supports image anaylsis
 										</Tooltip>
 									{/if}
 
